b16303c0e2078a164d9bdecd709d94c2ff3d058f
GianmariaFatta <gianmaria.fatta@gmail.com>
2022-12-07 17:04:18 +0100

    numbers from 1 to 100

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@
 // per i multipli di 3 stampi “Fizz” al posto del numero
 // per i multipli di 5 stampi “Buzz” al posto del numero.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
+
+
+//creiamo un programma che stampi in console numeri da 1 a 100
+    for (let i = 1 ; i <= 100; i ++){
+        console.log(i);
+
+    }
+
