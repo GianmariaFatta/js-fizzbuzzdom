e0abbb8dfa220f73d323f2b580ccac1d2afd99a3
GianmariaFatta <gianmaria.fatta@gmail.com>
2022-12-07 17:19:12 +0100

    end software

--- js/script.js
+++ js/script.js
@@ -6,7 +6,19 @@
 
 //creiamo un programma che stampi in console numeri da 1 a 100
     for (let i = 1 ; i <= 100; i ++){
-        console.log(i);
+        
 
+        if((i %3 === 0 )  && (i %5 === 0) ){
+            console.log("FizzBuzz")
+        }
+        else if( i %3 === 0){
+            console.log("fizz")
+        }
+        else if( i %5 === 0){
+            console.log("buzz")
+        }
+        else{
+            console.log(i)
+        }
     }
 
