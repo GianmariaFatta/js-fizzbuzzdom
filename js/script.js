46a3bdd4a9c1be8b316064bf5290bf6ad334fe57
GianmariaFatta <gianmaria.fatta@gmail.com>
2022-12-07 17:21:35 +0100

    Add comment

--- js/script.js
+++ js/script.js
@@ -7,15 +7,15 @@
 //creiamo un programma che stampi in console numeri da 1 a 100
     for (let i = 1 ; i <= 100; i ++){
         
-
+// sostituiamo con  i multipli di 3 “Fizz”, con i multipli di 5 “Buzz” e con i numeri che sono sia multipli di 3 che di 5 “FizzBuzz” 
         if((i %3 === 0 )  && (i %5 === 0) ){
             console.log("FizzBuzz")
         }
         else if( i %3 === 0){
-            console.log("fizz")
+            console.log("Fizz")
         }
         else if( i %5 === 0){
-            console.log("buzz")
+            console.log("Buzz")
         }
         else{
             console.log(i)
