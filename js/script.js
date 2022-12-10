49c71f850a386e72f10e152c62c7d42dcfc65ca2
GianmariaFatta <gianmaria.fatta@gmail.com>
2022-12-10 18:51:51 +0100

    program fixed

--- js/script.js
+++ js/script.js
@@ -13,22 +13,32 @@ let numberList =`<ul class= "d-flex m-4 flex-wrap " >`;
         
 // sostituiamo con  i multipli di 3 “Fizz”, con i multipli di 5 “Buzz” e con i numeri che sono sia multipli di 3 che di 5 “FizzBuzz” 
     
+            const feb="FizzBuzz"
+            const fz="Fizz"
+            const bz="Buzz"
 
-    if((i %3 === 0 )  && (i %5 === 0) ){
+if((i %3 === 0 )  && (i %5 === 0) ){
+    
             console.log("FizzBuzz");
+            numberList += `<li class="bg-white text-black"> ${feb} </li>`;
+
             
         }
         else if( i %3 === 0){
             console.log("Fizz")
+            numberList += `<li class="bg-danger "> ${fz} </li>`
+
         }
         else if( i %5 === 0){
             console.log("Buzz")
+            numberList += `<li class="bg-success"> ${bz} </li>`
         }
         else{
             console.log(i)
+            numberList += `<li class="bg-info text-black"> ${i} </li>`
         }
         
-        numberList += `<li> ${i} </li>`
+        
     }
 
      numberList +=`</ul>`;
